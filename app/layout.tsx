605134b8fd54ec0a60f74adbbc0778ad8eb7edf3
Matheus Escobar <matheus.escobar90@gmail.com>
2025-01-02 02:36:17 -0300

    (style): criar layout da sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,30 @@ import FlagRoundedIcon from "@mui/icons-material/FlagRounded";
 import AccountBalanceRoundedIcon from "@mui/icons-material/AccountBalanceRounded";
 import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
 import HelpOutlineRoundedIcon from "@mui/icons-material/HelpOutlineRounded";
+import React from "react";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Finx",
 };
+
+const routes = [
+  { path: "#", icon: <SsidChartRoundedIcon /> },
+  {
+    path: "#",
+    icon: <LocalAtmRoundedIcon />,
+  },
+  { path: "#", icon: <FlagRoundedIcon /> },
+  {
+    path: "#",
+    icon: <AccountBalanceRoundedIcon />,
+  },
+  {
+    path: "#",
+    icon: <SettingsRoundedIcon />,
+  },
+];
+const iconStyles = { width: 40, height: 40, color: "primary.contrastText" };
 
 export default function RootLayout({
   children,
@@ -26,25 +46,47 @@ export default function RootLayout({
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Stack sx={{ bgcolor: "primary.main", width: 100 }}>
-              <img src="/logo.svg" alt="Logo do Finx" />
-              <Link href="">
-                <SsidChartRoundedIcon />
-              </Link>
-              <Link href="">
-                <LocalAtmRoundedIcon />
-              </Link>
-              <Link href="">
-                <FlagRoundedIcon />
-              </Link>
-              <Link href="">
-                <AccountBalanceRoundedIcon />
-              </Link>
-              <Link href="">
-                <SettingsRoundedIcon />
-              </Link>
-              <Link href="">
-                <HelpOutlineRoundedIcon />
+            <Stack
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                bgcolor: "primary.main",
+                width: 80,
+                height: "100vh",
+                p: 1,
+              }}
+            >
+              <Image
+                src="/logo.svg"
+                width={60}
+                height={60}
+                alt="Logo do Finx"
+              />
+              <Stack
+                sx={{
+                  mt: 5,
+                  height: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: 2,
+                }}
+              >
+                {routes.map((route, index) => (
+                  <Link href={route.path} passHref key={index}>
+                    {React.cloneElement(route.icon, { sx: iconStyles })}
+                  </Link>
+                ))}
+              </Stack>
+              <Link href="#" passHref>
+                <HelpOutlineRoundedIcon
+                  sx={{
+                    width: 40,
+                    height: 40,
+                    color: "primary.contrastText",
+                    mt: "auto",
+                  }}
+                />
               </Link>
             </Stack>
             {children}
